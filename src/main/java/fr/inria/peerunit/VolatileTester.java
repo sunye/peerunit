cbaddc97475a047bf26f21e2b5b40c897639ed82
booba <booba@527d5a51-f140-0410-ba99-e037b0884c56>
2009-03-31 09:35:29 +0000

    javadoc

--- src/main/java/fr/inria/peerunit/VolatileTester.java
+++ src/main/java/fr/inria/peerunit/VolatileTester.java
@@ -1,5 +1,20 @@
 package fr.inria.peerunit;
 
+/**
+ * This interface define a volatile <i>tester</i>, more  a <i>tester</i> who can be stopped 
+ * during the testing.
+ * 
+ * @author Eduardo Almeida
+ * @author Aboubakar Koïta
+ * @version 1.0
+ * @since 1.0 
+ * @see fr.inria.peerunit.StorageTester
+ * @see fr.inria.peerunit.rmi.tester.TesterImpl 
+ */
 public interface VolatileTester {
+	
+	/**
+	 * Stop the <i>tester</i>.
+	 */	
 	public void kill() ;
 }
