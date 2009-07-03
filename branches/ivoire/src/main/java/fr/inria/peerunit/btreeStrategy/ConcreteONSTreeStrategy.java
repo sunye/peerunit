e7a18129e7ca228c11cece5729d8f839ac9a0006
booba <booba@527d5a51-f140-0410-ba99-e037b0884c56>
2009-07-03 14:10:17 +0000

    remplacement de l'adresse de la station d construction de l'arbre des testeurs par cette adresse=arbreTesterUtil.instance.getOnStationRoot()

--- branches/ivoire/src/main/java/fr/inria/peerunit/btreeStrategy/ConcreteONSTreeStrategy.java
+++ branches/ivoire/src/main/java/fr/inria/peerunit/btreeStrategy/ConcreteONSTreeStrategy.java
@@ -19,10 +19,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 import fr.inria.peerunit.btree.Node;
 import fr.inria.peerunit.btree.TreeElements;
 import fr.inria.peerunit.onstree.stationTree.Station;
+import fr.inria.peerunit.onstree.stationTree.StationContainer;
 import fr.inria.peerunit.onstree.stationTree.StationRoot;
 import fr.inria.peerunit.onstree.stationTree.StationTree;
 import fr.inria.peerunit.onstree.stationTree.StationTreeBuilder;
-import fr.inria.peerunit.onstree.testerTree.TesterNodeHead_be;
+import fr.inria.peerunit.onstree.testerTree.TesterNodeHead;
 import fr.inria.peerunit.onstree.testerTree.TesterTreeBuilder;
 import fr.inria.peerunit.util.TesterUtil;
 
@@ -30,15 +31,17 @@ import fr.inria.peerunit.util.TesterUtil;
  * This strategy allow to use the Optimized Network Station Tree
  * 
  * @author jeremy
- * @author Aboubakar Koïta 
+ * @author Aboubakar Koïta
  * 
  */
 public class ConcreteONSTreeStrategy implements TreeStrategy {
-	private TesterNodeHead_be testerNH;
-	private HashMap<String, TesterNodeHead_be> ipNodeHeadMap = new HashMap<String, TesterNodeHead_be>();
+	private TesterNodeHead testerNH;
+	private HashMap<String, TesterNodeHead> ipNodeHeadMap = new HashMap<String, TesterNodeHead>();
 	private HashMap<String, Node> remotesNodesMap = new HashMap<String, Node>();
 	private AtomicInteger registered = new AtomicInteger(0);
-	//private static int expectedTesters = TesterUtil.instance.getExpectedTesters();
+
+	// private static int expectedTesters =
+	// TesterUtil.instance.getExpectedTesters();
 
 	/**
 	 * Constructor by default
@@ -53,24 +56,25 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 	 * @see fr.inria.peerunit.btreeStrategy.TreeStrategy#buildTree()
 	 */
 	public void buildTree() {
-		String hostsFile = TesterUtil.instance.getHostsFilePath();
 		InputStream input = ConcreteONSTreeStrategy.class
-				.getResourceAsStream(hostsFile);
+				.getResourceAsStream("/hosts.txt");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 		String host = null;
 		List<Station> listStation = new ArrayList<Station>();
 		int cpt = 1;
-/*		try {
+		try {
 			while ((host = reader.readLine()) != null) {
-				Station station = new Station(MTRLib.getRoute(host), host,
-						Integer.toString(cpt++));
+				String route=traceroute.TraceRoute.getRoute("-n", host);
+				Station station = new Station(route,host, Integer.toString(cpt++));
 				listStation.add(station);
+				
+				System.out.println("Route="+route);
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		}*/
+		}
 		// Rooter tree
-		StationRoot stRoot = new StationRoot("192.168.1.14", "BoutStrap");
+		StationRoot stRoot = new StationRoot(TesterUtil.instance.getOnStationRoot(), "Bootstrap");
 		StationTreeBuilder builder = new StationTreeBuilder();
 		builder.buildNetTree(listStation, stRoot);
 
@@ -80,7 +84,6 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 		if (TesterUtil.instance.getStationTreeTrace() == 1) {
 			stTree.print();
 		}
-
 		// tester tree
 		TesterTreeBuilder testTreeBuilder = new TesterTreeBuilder();
 		// parameter tester number = 165 for 11 stations (15 tester max by
@@ -88,7 +91,7 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 		testerNH = testTreeBuilder.buildTesterTree(stTree, TesterUtil.instance
 				.getExpectedTesters());
 		buildIPNodeHeadMap(testerNH);
-
+		printStTree(stTree);
 		if (TesterUtil.instance.getStationTreeTrace() == 1) {
 			try {
 				PrintWriter out = new PrintWriter(new FileWriter(
@@ -109,7 +112,7 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 				out.println(testerNH.graphicPrintTester());
 				out.println("}");
 				out.close();
-
+				
 				// génération des images
 				Runtime.getRuntime().exec(
 						"dot -Tpng routersTree.txt -o RoutersGraph.png");
@@ -125,11 +128,18 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 
 	}
 
-	private void buildIPNodeHeadMap(TesterNodeHead_be tester) {
+	private void buildIPNodeHeadMap(TesterNodeHead tester) {
 		ipNodeHeadMap.put(tester.getIP(), tester);
-		for (TesterNodeHead_be nodeHeadBe : tester.getListTesterNodeHead()) {
+		System.out.println("NodeHeapIP="+tester.getIP());
+		System.out.println("childs");		
+		for (TesterNodeHead nodeHeadBe : tester.getListTesterNodeHead()) {
+			System.out.println(nodeHeadBe.getIP());			
+		}
+		System.out.println("\n\n");
+		for (TesterNodeHead nodeHeadBe : tester.getListTesterNodeHead()) {
 			buildIPNodeHeadMap(nodeHeadBe);
 		}
+		
 	}
 
 	/*
@@ -172,7 +182,7 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 
 	public int register(Node node) throws RemoteException {
 		remotesNodesMap.put(node.getIP(), node);
-		TesterNodeHead_be nodeHeadBe = ipNodeHeadMap.get(node.getIP());
+		TesterNodeHead nodeHeadBe = ipNodeHeadMap.get(node.getIP());
 		registered.incrementAndGet();
 		return nodeHeadBe.getId();
 	}
@@ -183,37 +193,57 @@ public class ConcreteONSTreeStrategy implements TreeStrategy {
 
 	public void setCommunication() {
 		for (String key : ipNodeHeadMap.keySet()) {
+			System.out.println("Current Key="+key);
 			TreeElements te = new TreeElements();
 			AbstractBTreeNode node = getNode(key);
 			if (!node.isLeaf()) {
-				List<TesterNodeHead_be> chilNhead = ((TesterNodeHead_be) getNode(key))
-						.getListTesterNodeHead();
-				for (TesterNodeHead_be nodeHeadBe : chilNhead) {
+				TesterNodeHead nodeHead=(TesterNodeHead)getNode(key);
+				List<TesterNodeHead> childsNheads =nodeHead.getListTesterNodeHead();
+				System.out.println("Setcommunication NodeHead="+nodeHead.getIP());				
+				for (TesterNodeHead nodeHeadBe : childsNheads) {
+					System.out.println("Setcommunication Childs="+nodeHeadBe.getIP());					
 					if (nodeHeadBe != null) {
 						Node node2 = remotesNodesMap.get(nodeHeadBe.getIP());
 						te.setChildren(node2);
-					} else {
-						te.setChildren(null);
 					}
 				}
-				if (!node.isRoot()) {
-					TesterNodeHead_be parent = (TesterNodeHead_be) node
-							.getParent();
-					te.setParent(remotesNodesMap.get(parent.getIP()));
-				}
-				/**
-				 * Now we inform Node its tree elements.
-				 */
-				Node remoteNode = remotesNodesMap.get(key);
-				System.out.println("[Bootstrapper] Contacting Node "
-						+ remoteNode);
-				try {
-					remoteNode.setElements(getNode(key), te);
-				} catch (RemoteException e) {
-					e.printStackTrace();
-				}
+				System.out.println("\n\n");
+			} else {
+				te.setChildren(null);
+			}
+			if (!node.isRoot()) {
+				TesterNodeHead parent = (TesterNodeHead) node.getParent();
+				te.setParent(remotesNodesMap.get(parent.getIP()));
+			}
 
+			System.out.println("[Bootstrapper] Befor Contacting Node ");
+			/**
+			 * Now we inform Node its tree elements.
+			 */
+			Node remoteNode = remotesNodesMap.get(key);
+			System.out.println("[Bootstrapper] Contacting Node " + remoteNode);
+			try {
+				remoteNode.setElements(getNode(key), te);
+			} catch (RemoteException e) {
+				e.printStackTrace();
 			}
 		}
 	}
+
+    private void printStTree(fr.inria.peerunit.onstree.stationTree.StationContainer station)
+    {
+		System.out.println("Station IP="+station.getStation().getIp());
+    	for(fr.inria.peerunit.onstree.stationTree.Node node:station.getListChildStation())
+    	{
+    		System.out.println("Station child IP="+node.getStParent().getIp());    		
+    	}
+
+    	
+    	System.out.println("\n\n");
+    	
+    	for(fr.inria.peerunit.onstree.stationTree.Node node:station.getListChildStation())
+    	{
+    		printStTree(node);
+    	}	
+    }
 }
