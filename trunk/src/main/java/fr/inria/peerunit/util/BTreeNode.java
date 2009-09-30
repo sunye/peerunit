f1b4a01b226ce621ce91d4785442f50518e2a78b
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2009-09-30 22:10:51 +0000

    new classes

---
+++ trunk/src/main/java/fr/inria/peerunit/util/BTreeNode.java
@@ -0,0 +1,17 @@
+package fr.inria.peerunit.util;
+
+/**
+ * 
+ * @author Veronique Pelleau
+ * @version 1.0
+ * @since 1.0
+ */
+public interface BTreeNode {
+	
+	 public int getId();
+	 public Comparable[] getKeys();
+	 public BTreeNode getParent();
+	 public BTreeNode[] getChildren();
+	 public boolean isLeaf();
+	 public boolean isRoot();
+}
