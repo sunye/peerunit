baf996859f089487709166795cefb97e186f3443
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2009-09-30 22:14:01 +0000

    remove useless class.

--- src/main/java/fr/inria/peerunit/btreeStrategy/ConcreteBtreeStrategy.java
+++ src/main/java/fr/inria/peerunit/btreeStrategy/ConcreteBtreeStrategy.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import fr.inria.peerunit.btree.BTree;
+import fr.inria.peerunit.util.BTreeImpl;
+import fr.inria.peerunit.util.BTreeNode;
 import fr.inria.peerunit.btree.Node;
 import fr.inria.peerunit.btree.TreeElements;
 import fr.inria.peerunit.util.TesterUtil;
@@ -14,88 +15,80 @@ import fr.inria.peerunit.util.TesterUtil;
  * @author Veronique Pelleau
  * @author Aboubakar Koïta
  */
-
 public class ConcreteBtreeStrategy implements TreeStrategy {
 
-	
-	private BTree btree;
-	
-	private AtomicInteger registered = new AtomicInteger(0);
-	
-	//private static int expectedTesters=TesterUtil.instance.getExpectedTesters();
-	
-	private Map<Integer,Node> nodes = new HashMap<Integer,Node>();	
-	
-	//private static final Logger log = Logger.getLogger(CoordinatorImpl.class.getName());
-	
-	//private static Long time;	
-	
-	
-	public ConcreteBtreeStrategy() {
-		btree = new BTree(TesterUtil.instance.getTreeOrder());
-	}
-	
-	public void buildTree() {
-		btree.buildTree();
-	}
+    private BTreeImpl btree;
+    private AtomicInteger registered = new AtomicInteger(0);
+    //private static int expectedTesters=TesterUtil.instance.getExpectedTesters();
+    private Map<Integer, Node> nodes = new HashMap<Integer, Node>();
 
-	public AbstractBTreeNode getNode(Integer i) {
-		return btree.getNode(i);
-	}
+    //private static final Logger log = Logger.getLogger(CoordinatorImpl.class.getName());
+    //private static Long time;
+    public ConcreteBtreeStrategy() {
+        btree = new BTreeImpl(TesterUtil.instance.getTreeOrder());
+    }
 
-	public int getNodesSize() {
-		return btree.nodes.size();
-	}
+    public void buildTree() {
+        btree.buildTree();
+    }
 
-	public AbstractBTreeNode getNode(Object key) {  // XXX
-		return getNode((Integer)key);
-	}
-	
-	public void setCommunication(){	
-		Node node;			
-		
-		for(Integer key:nodes.keySet()){
-			TreeElements te=new TreeElements();	
-			if(!getNode(key).isLeaf()){
-				for(AbstractBTreeNode child:getNode(key).getChildren()){
-					if(child!=null){
-						te.setChildren(nodes.get(child.getId()));
-					}
-				}
-			}else
-			{
-				te.setChildren(null);
-			}
-			
-			if(!getNode(key).isRoot()){
-				int parentId=getNode(key).getParent().getId();
-				te.setParent(nodes.get(parentId));
-			}
-			/**
-			 * Now we inform Node its tree elements. 
-			 */			
-			node=nodes.get(key);
-			System.out.println("[Bootstrapper] Contacting Node "+node);
-			try {
-				node.setElements(getNode(key),te);
-			} catch (RemoteException e) {				
-				e.printStackTrace();
-			}
-		}
-	}
-	public int register(Node node) throws RemoteException {
-		int id = registered.getAndIncrement();		
-		if(id<getNodesSize()){
-			nodes.put(id, node);
-			System.out.println("[Bootstrapper] New Registered ID: " + id+" for "+node);
-			return id;
-		}else{
-			System.out.println("[Bootstrapper] Not registerd " + id+" for "+node);
-			return Integer.MAX_VALUE;
-		}		
-	}
+    public BTreeNode getNode(Integer i) {
+        return btree.getNode(i);
+    }
 
-	public int getRegistered() {
-		return registered.intValue();
-	}
+    public int getNodesSize() {
+        return btree.getNodesSize();
+    }
+
+    public BTreeNode getNode(Object key) {  // XXX
+        return getNode((Integer) key);
+    }
+
+    public void setCommunication() {
+        Node node;
+
+        for (Integer key : nodes.keySet()) {
+            TreeElements te = new TreeElements();
+            if (!getNode(key).isLeaf()) {
+                for (BTreeNode child : getNode(key).getChildren()) {
+                    if (child != null) {
+                        te.setChildren(nodes.get(child.getId()));
+                    }
+                }
+            } else {
+                te.setChildren(null);
+            }
+
+            if (!getNode(key).isRoot()) {
+                int parentId = getNode(key).getParent().getId();
+                te.setParent(nodes.get(parentId));
+            }
+            /**
+             * Now we inform Node its tree elements.
+             */
+            node = nodes.get(key);
+            System.out.println("[Bootstrapper] Contacting Node " + node);
+            try {
+                node.setElements(getNode(key), te);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int register(Node node) throws RemoteException {
+        int id = registered.getAndIncrement();
+        if (id < getNodesSize()) {
+            nodes.put(id, node);
+            System.out.println("[Bootstrapper] New Registered ID: " + id + " for " + node);
+            return id;
+        } else {
+            System.out.println("[Bootstrapper] Not registerd " + id + " for " + node);
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    public int getRegistered() {
+        return registered.intValue();
+    }
 }
