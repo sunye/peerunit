1a8c6db173aee52a016647e35cf84af94becd4ff
pvaillant <pvaillant@527d5a51-f140-0410-ba99-e037b0884c56>
2010-02-25 02:34:36 +0000

    Quelques tests...

--- RMILite/src/test/java/com/alma/rmilite/RemoteObjectImpl.java
+++ RMILite/src/test/java/com/alma/rmilite/RemoteObjectImpl.java
@@ -1,10 +1,12 @@
 package com.alma.rmilite;
 
+import java.rmi.RemoteException;
+
 public class RemoteObjectImpl implements RemoteObject {
-	
+		
 	private int nbCall = 0;
 
-	public int getNbCall() {
+	public int getNbCall() throws RemoteException {
 		return ++nbCall;
 	}
 }
