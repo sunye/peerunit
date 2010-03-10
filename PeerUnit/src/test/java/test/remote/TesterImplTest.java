7b2100db9c20c51c6ed95b6cd588ac34654b268e
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2010-03-10 23:29:13 +0000

    Still trying to solve blocking tests.

--- PeerUnit/src/test/java/test/remote/TesterImplTest.java
+++ PeerUnit/src/test/java/test/remote/TesterImplTest.java
@@ -1,21 +1,23 @@
 /*
-    This file is part of PeerUnit.
+This file is part of PeerUnit.
 
-    PeerUnit is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+PeerUnit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    PeerUnit is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+PeerUnit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with PeerUnit.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with PeerUnit.  If not, see <http://www.gnu.org/licenses/>.
  */
 package test.remote;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -35,6 +37,7 @@ import fr.inria.peerunit.base.Sample;
 import fr.inria.peerunit.common.MethodDescription;
 import fr.inria.peerunit.coordinator.CoordinatorImpl;
 import fr.inria.peerunit.remote.Bootstrapper;
+import fr.inria.peerunit.remote.Coordinator;
 import fr.inria.peerunit.remote.GlobalVariables;
 import fr.inria.peerunit.remote.Tester;
 import fr.inria.peerunit.tester.TestCaseWrapper;
@@ -43,53 +46,65 @@ import fr.inria.peerunit.util.TesterUtil;
 
 public class TesterImplTest {
 
-    private static TestCaseWrapper executor;
+    private static TestCaseWrapper wrapper;
     private static CoordinatorImpl coord;
-    private static GlobalVariables globals; 
+    private static Coordinator remoteCoordinator;
+    private static Bootstrapper bootstrapper;
+    private static GlobalVariables globals;
     private static TesterImpl tester0, tester1, tester2;
 
     /**
      * FIXME Tests not working correctly in all platforms.
      * @throws InterruptedException
      */
-    //@BeforeClass
-    public static void inititalize() throws InterruptedException {
-        Properties properties = new Properties();
+    @BeforeClass
+    public static void inititalize() throws InterruptedException, RemoteException {
+        // Coordinator initialization
+        coord = new CoordinatorImpl(3, 100);
+        remoteCoordinator = coord.getRemoteCoordinator();
+        bootstrapper = coord.getRemoteBootstrapper();
+        new Thread(coord, "Coordinator").start();
+        Thread.yield();
 
-        properties.setProperty("tester.peers", "3");
-        properties.setProperty("tester.log.dateformat", "yyyy-MM-dd");
-        properties.setProperty("tester.log.timeformat", "HH:mm:ss.SSS");
-        properties.setProperty("tester.log.level", "FINEST");
-        properties.setProperty("tester.logfolder", "/tmp/");
-        properties.setProperty("tester.log.delimiter", "|");
-        properties.setProperty("tester.waitForMethod", "500");
-        try {
-            TesterUtil defaults = new TesterUtil(properties);
-            coord = new CoordinatorImpl(3, 100);
-            globals = new GlobalVariablesImpl();
-            new Thread(coord, "Coordinator").start();
-            tester0 = new TesterImpl(coord.getRemoteBootstrapper(), globals);
-            tester0.setCoordinator(coord.getRemoteCoordinator());
-            executor = new TestCaseWrapper(tester0);
-            tester1 = new TesterImpl(coord.getRemoteBootstrapper(), globals);
-            tester1.setCoordinator(coord.getRemoteCoordinator());
-            tester2 = new TesterImpl(coord.getRemoteBootstrapper(), globals);
-            tester2.setCoordinator(coord.getRemoteCoordinator());
-            tester0.registerTestCase(Sample.class);
-            tester1.registerTestCase(Sample.class);
-            tester2.registerTestCase(Sample.class);
-            Thread.yield();
+        // Testers initialization
+        globals = new GlobalVariablesImpl();
+        tester0 = new TesterImpl(bootstrapper, globals);
+        tester1 = new TesterImpl(bootstrapper, globals);
+        tester2 = new TesterImpl(bootstrapper, globals);
 
-            tester0.start();
-            tester1.start();
-            tester2.start();
-            
-            coord.waitForTesterRegistration();
-           
-            //new Thread(tester0, "Tester 0").start();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        tester0.setCoordinator(remoteCoordinator);
+        tester1.setCoordinator(remoteCoordinator);
+        tester2.setCoordinator(remoteCoordinator);
+
+        tester0.registerTestCase(Sample.class);
+        tester1.registerTestCase(Sample.class);
+        tester2.registerTestCase(Sample.class);
+
+
+        wrapper = new TestCaseWrapper(tester0);
+
+        Thread.yield();
+
+        tester0.start();
+        tester1.start();
+        tester2.start();
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    coord.waitForTesterRegistration();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(TesterImplTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+
+        t.start();
+        t.join();
+
+        //new Thread(tester0, "Tester 0").start();
+
     }
 
     //@Test
@@ -102,9 +117,9 @@ public class TesterImplTest {
         fail("Not yet implemented");
     }
 
-    //@Test
+    @Test
     public void testRegister() {
-        MethodDescription md = new MethodDescription("first", 1, 1000); 
+        MethodDescription md = new MethodDescription("first", 1, 1000);
 
         assertEquals(3, coord.getSchedule().size());
 
@@ -115,16 +130,19 @@ public class TesterImplTest {
 
     }
 
+    /**
+     * FIXME
+     */
     //@Test
     public void testExecute() {
         try {
             Bootstrapper boot = mock(Bootstrapper.class);
             TesterImpl tester = new TesterImpl(boot, globals);
-            tester.setCoordinator(coord.getRemoteCoordinator());
+            tester.setCoordinator(remoteCoordinator);
             tester.registerTestCase(Sample.class);
 
             System.setProperty("executed", "nok");
-            List<MethodDescription> methods = executor.register(Sample.class);
+            List<MethodDescription> methods = wrapper.register(Sample.class);
             Thread tt = new Thread(tester, "Test Execute");
             tt.start();
 
@@ -143,22 +161,11 @@ public class TesterImplTest {
 
     }
 
-    //@Test
-    public void testGetPeerName() {
+    @Test
+    public void testGetId() {
         assertEquals(0, tester0.getId());
         assertEquals(1, tester1.getId());
         assertEquals(2, tester2.getId());
-    }
-
-    //@Test
-    public void testGetId() {
-        try {
-            assertEquals(0, tester0.getPeerName());
-            assertEquals(1, tester1.getPeerName());
-            assertEquals(2, tester2.getPeerName());
-        } catch (RemoteException e) {
-            fail("Communication error");
-        }
     }
 
     //@Test
@@ -166,7 +173,7 @@ public class TesterImplTest {
         fail("Not yet implemented");
     }
 
-    //@Test
+    @Test
     public void testPut() {
         try {
             tester0.put(0, "zero");
@@ -201,7 +208,7 @@ public class TesterImplTest {
         fail("Not yet implemented");
     }
 
-    //@Test
+    @Test
     public void mysets() {
         Set<Tester> myset = new HashSet<Tester>();
 
