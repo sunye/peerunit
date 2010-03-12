c4e41b257e0d11048f4572d627bba3621477543c
guillaumem2alma <guillaumem2alma@527d5a51-f140-0410-ba99-e037b0884c56>
2010-03-12 00:36:39 +0000

    suppression des warnings

--- trunk/RMILite/src/test/java/nio/ClientAndServerTest.java
+++ trunk/RMILite/src/test/java/nio/ClientAndServerTest.java
@@ -15,7 +15,6 @@ import nio.server.ByteArrayHandler;
 import nio.server.ByteSocketServer;
 
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -28,11 +27,6 @@ public class ClientAndServerTest {
 	protected final static int listeningPort = 1112;
 	protected List<Object> receivedObjects;
 	protected int nb_received = 0;
-
-	@BeforeClass
-	public void setUpClass() {
-
-	}
 
 	@BeforeMethod
 	public void setUp() throws IOException {
