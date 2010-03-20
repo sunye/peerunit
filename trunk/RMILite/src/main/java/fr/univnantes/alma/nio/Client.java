83e7494ef8836f24fcfc23b0664d28eb1c6ec717
guillaumem2alma <guillaumem2alma@527d5a51-f140-0410-ba99-e037b0884c56>
2010-03-20 06:45:58 +0000

    optimization of the handling part.

--- trunk/RMILite/src/main/java/fr/univnantes/alma/nio/Client.java
+++ trunk/RMILite/src/main/java/fr/univnantes/alma/nio/Client.java
@@ -1,7 +1,7 @@
 package fr.univnantes.alma.nio;
 
 /**
- * A nio client is at the same time a {@link Reader} readerand a {@link Writer}
+ * A nio client is at the same time a {@link Reader} and a {@link Writer}
  * 
  * @author Guillaume Le Louët
  * 
