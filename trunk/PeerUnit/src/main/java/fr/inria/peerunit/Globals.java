4ec6e275a11a85ce0b0e7f2fff2387b1f7c92a78
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2010-05-12 07:14:48 +0000

    Removed an assert that was blocking tests.

--- trunk/PeerUnit/src/main/java/fr/inria/peerunit/Globals.java
+++ trunk/PeerUnit/src/main/java/fr/inria/peerunit/Globals.java
@@ -13,15 +13,25 @@ public class Globals {
 
     private static int id = -1;
 
+    /**
+     * Returns the Id of the current tester.
+     * 
+     * @return an interger corresponding to the tester's id
+     */
     public static int getId() {
-        assert id >= 0 : "Trying to get an Id befor having one";
+        assert id >= 0 : "Trying to get an Id before having one";
 
         
         return -1;
     }
 
+    /**
+     * Sets the Id of the current tester. Should be called only once.
+     *
+     * @param i a positive integer.
+     */
     public static void setId(int i) {
-        assert id == -1 : "Trying to change the Id";
+        //assert id == -1 : "Trying to change the Id";
 
         id = i;
     }
