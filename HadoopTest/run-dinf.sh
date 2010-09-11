6997b7a668f9a11183d190cdd9071498a90df029
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2010-09-11 00:36:53 +0000

    run-dinf.sh

---
+++ HadoopTest/run-dinf.sh
@@ -0,0 +1,15 @@
+# Coordenador
+java -classpath .:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+# Bootstap OpenChord
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar:~/openchord/dist/openchord_1.0.5.jar openchord.Bootstrap &
+
+# Testers
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar fr.inria.peerunit.TestRunner test.SimpleTest &
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar fr.inria.peerunit.TestRunner test.SimpleTest &
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar fr.inria.peerunit.TestRunner test.SimpleTest &
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar fr.inria.peerunit.TestRunner test.SimpleTest &
+#java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar fr.inria.peerunit.TestRunner test.SimpleTest &
+#~/hadoop-0.20.2/bin/hadoop jobtracker
+
+# Teste
+/usr/lib/jvm/java-6-sun/bin/java -classpath .:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar:./target/HadoopTest-1.0-SNAPSHOT.jar:~/.m2/repository/org/apache/hadoop/hadoop-core/0.20.2/hadoop-core-0.20.2.jar fr.inria.peerunit.TestRunner load.TestStartCluster /dirin/ /dirout/
