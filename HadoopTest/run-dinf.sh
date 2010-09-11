31a9a66ba4b79b09b5f2cdc63722bab03c21ceda
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2010-09-11 06:44:13 +0000

    Start JobTracker

--- HadoopTest/run-dinf.sh
+++ HadoopTest/run-dinf.sh
@@ -1,5 +1,5 @@
 # Coordenador
-java -classpath .:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
 # Bootstap OpenChord
 #java -classpath .:target/PeerUnit-1.0-SNAPSHOT.jar:~/openchord/dist/openchord_1.0.5.jar openchord.Bootstrap &
 
@@ -12,4 +12,6 @@ java -classpath .:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit
 #~/hadoop-0.20.2/bin/hadoop jobtracker
 
 # Teste
-/usr/lib/jvm/java-6-sun/bin/java -classpath .:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar:./target/HadoopTest-1.0-SNAPSHOT.jar:~/.m2/repository/org/apache/hadoop/hadoop-core/0.20.2/hadoop-core-0.20.2.jar fr.inria.peerunit.TestRunner load.TestStartCluster /dirin/ /dirout/
+~/jdk1.6.0_21/bin/java -Dhadoop.log.dir=./log/ -Dhadoop.log.file=hadoop -classpath ~/.m2/repository/org/apache/hadoop/hadoop-core/0.20.2/hadoop-core-0.20.2.jar:./lib/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar:./target/HadoopTest-1.0-SNAPSHOT.jar:./lib/org/mortbay/jetty/jetty/6.1.25/jetty-6.1.25.jar:./lib/org/mortbay/jetty/jetty-util/6.1.25/jetty-util-6.1.25.jar:./lib/org/mortbay/jetty/servlet-api/2.5-20081211/servlet-api-2.5-20081211.jar:./lib/commons-httpclient/commons-httpclient/3.1/commons-httpclient-3.1.jar fr.inria.peerunit.TestRunner load.TestStartCluster &
+
+~/jdk1.6.0_21/bin/java -Dhadoop.log.dir=./log/ -Dhadoop.log.file=hadoop -classpath ~/.m2/repository/org/apache/hadoop/hadoop-core/0.20.2/hadoop-core-0.20.2.jar:./lib/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar:../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar:./target/HadoopTest-1.0-SNAPSHOT.jar:./lib/org/mortbay/jetty/jetty/6.1.25/jetty-6.1.25.jar:./lib/org/mortbay/jetty/jetty-util/6.1.25/jetty-util-6.1.25.jar:./lib/org/mortbay/jetty/servlet-api/2.5-20081211/servlet-api-2.5-20081211.jar:./lib/commons-httpclient/commons-httpclient/3.1/commons-httpclient-3.1.jar fr.inria.peerunit.TestRunner load.TestStartCluster &
