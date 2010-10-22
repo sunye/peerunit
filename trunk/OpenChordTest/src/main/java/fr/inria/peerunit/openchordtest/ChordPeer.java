8e3448ab4d5653245f775478f92b7d9cc2e94b0b
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2010-10-22 11:42:29 +0000

    Routing table test.

--- trunk/OpenChordTest/src/main/java/fr/inria/peerunit/openchordtest/ChordPeer.java
+++ trunk/OpenChordTest/src/main/java/fr/inria/peerunit/openchordtest/ChordPeer.java
@@ -2,15 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package fr.inria.peerunit.openchordtest;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import de.uniba.wiai.lspi.chord.data.URL;
@@ -24,96 +23,98 @@ import fr.inria.peerunit.dhtmodel.Peer;
  * @author sunye
  */
 public class ChordPeer implements Peer {
-	private static final Logger LOG = Logger.getLogger(ChordPeer.class
-			.getName());
-	private ChordImpl node;
-	private URL bootstrapURL;
-	private URL localURL;
 
-	public ChordPeer(InetSocketAddress address) throws IOException {
-		PropertiesLoader.loadPropertyFile();
-		String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-		bootstrapURL = new URL(protocol + "://" + address.getHostName() + ":" + address.getPort() + "/");
-		node = new ChordImpl();
-		
-		LOG.info("property file loaded");
-	}
+    private static final Logger LOG = Logger.getLogger(ChordPeer.class.getName());
+    private ChordImpl node;
+    private URL bootstrapURL;
+    private URL localURL;
+    private final String protocol;
 
-	public boolean bootsrap() throws InterruptedException, IOException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public ChordPeer(InetSocketAddress address) throws IOException {
+        PropertiesLoader.loadPropertyFile();
+        protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
+        bootstrapURL = new URL(protocol + "://" + address.getHostName() + ":" + address.getPort() + "/");
+        node = new ChordImpl();
 
-	public String get(String key) throws InterruptedException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+        LOG.info("property file loaded");
+    }
 
-	public void put(String key, String value) throws InterruptedException {
-		StringKey chordKey = new StringKey(key);
-		node.insert(chordKey, value, null);
-	}
+    public boolean bootsrap() throws Exception {
+        node = new ChordImpl();
+        node.create(bootstrapURL);
+        return true;
+    }
 
-	public String getId() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public String get(String key) throws InterruptedException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-	public int getPort() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public void put(String key, String value) throws InterruptedException {
+        StringKey chordKey = new StringKey(key);
+        node.insert(chordKey, value, null);
+    }
 
-	public Set<String> getRoutingTable() {
-		 String[] successors = node.printSuccessorList().split("\n");
-		 return new HashSet<String>(Arrays.asList(successors));
-	}
+    public String getId() {
+        String result = node.getID().toHexString(4).trim();
+        LOG.info("Id size = " + result.length());
 
-	public boolean join() throws Exception {
-		FreeLocalPort port = new FreeLocalPort();
-		int freePort = port.getPort();
-		String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-		String address = InetAddress.getLocalHost().toString();
-		address = address.substring(address.indexOf("/") + 1, address.length());
+        return result;
+    }
 
-		localURL = new URL(protocol + "://" + address + ":" + freePort + "/");
+    public int getPort() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-		node.join(localURL, bootstrapURL);
-		return true;
-	}
+    public Set<String> getRoutingTable() {
+        Set<String> result = new HashSet<String>();
 
-	public void leave() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+        String[] successors = node.printSuccessorList().split("\n");
 
-	// public void join(AbstractOpenChordTest abstractOpenChordTest) throws
-	// ServiceException, UnknownHostException, MalformedURLException,
-	// InterruptedException {
-	//
-	//
-	// String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
-	// LOG.info("Peer name " + abstractOpenChordTest.getId());
-	// String address = InetAddress.getLocalHost().toString();
-	// address = address.substring(address.indexOf("/") + 1, address.length());
-	// FreeLocalPort port = new FreeLocalPort();
-	// LOG.info("Address: " + address + " on port " + port.getPort());
-	// abstractOpenChordTest.localURL = new URL(protocol + "://" + address + ":"
-	// + port.getPort() + "/");
-	// URL bootstrapURL = new URL(protocol + "://" +
-	// abstractOpenChordTest.defaults.getBootstrap() + ":" +
-	// abstractOpenChordTest.defaults.getBootstrapPort() + "/");
-	// abstractOpenChordTest.chord = new ChordImpl();
-	// Thread.sleep(100 * abstractOpenChordTest.getId());
-	// LOG.info("LocalURL: " + abstractOpenChordTest.localURL.toString());
-	// abstractOpenChordTest.getChord().join(abstractOpenChordTest.localURL,
-	// bootstrapURL);
-	// LOG.info("Joining Chord DHT: " +
-	// abstractOpenChordTest.getChord().toString());
-	// LOG.info("Peer init");
-	// }
+        for(int i = 1 ; i < successors.length ; i++) {
+            String entry = successors[i].split(",")[0].trim();
+            result.add(entry);
+            LOG.fine(entry);
+        }
 
-	public void print() {
-		LOG.info("My ID is " + this.getId());
-		String[] succ = node.printSuccessorList().split("\n");
-		for (String succList : succ) {
-			LOG.info("Successor List " + succList + " size " + succ.length);
-		}
+        return result;
+    }
 
-	}
+    public boolean join() throws Exception {
+        FreeLocalPort port = new FreeLocalPort();
+        int freePort = port.getPort();
+        String address = InetAddress.getLocalHost().toString();
+        address = address.substring(address.indexOf("/") + 1, address.length());
+
+        localURL = new URL(protocol + "://" + address + ":" + freePort + "/");
+
+        node.join(localURL, bootstrapURL);
+        return true;
+    }
+
+    public void leave() {
+        node.leave();
+    }
+
+    public void print() {
+        LOG.log(Level.INFO, "My ID is {0}", this.getId());
+
+        for (String each : this.getRoutingTable()) {
+            LOG.info("Successor entry: " + each + each.length());
+        }
+
+        for (String each : node.printFingerTable().split("\n")) {
+            LOG.info("Finger table entry:"+ each);
+        }
+
+
+        LOG.info("FINGER TABLE:");
+        LOG.info("===========================================");
+        LOG.info(node.printFingerTable());
+        LOG.info("===========================================");
+
+        LOG.info("===========================================");
+        LOG.info("SUCCESSOR TABLE:");
+        LOG.info(node.printSuccessorList());
+        LOG.info("===========================================");
+    }
 }
--- trunk/OpenChordTest/src/main/java/fr/inria/peerunit/openchordtest/StringKey.java
+++ trunk/OpenChordTest/src/main/java/fr/inria/peerunit/openchordtest/StringKey.java
@@ -1,26 +1,33 @@
 package fr.inria.peerunit.openchordtest;
 
+import de.uniba.wiai.lspi.chord.service.Key;
 
 /*
- * Class implemented by the authors of the Open Chord
+ * @author sunye
  */
-public class StringKey implements de.uniba.wiai.lspi.chord.service.Key {
-	private String theString; 	
-	
-	public StringKey(String theString){
-		this.theString = theString; 
-	}
-	public byte[] getBytes(){
-		return this.theString.getBytes(); 
-	}
-	public int hashCode(){
-		return this.theString.hashCode(); 
-	}
-	public boolean equals(Object o){
-		if (o instanceof StringKey){
-			((StringKey) o).theString.equals(this.theString);
-		} 
-		return false;
-	}
-	
+public class StringKey implements Key {
+
+    private String image;
+
+    public StringKey(String str) {
+        this.image = str;
+    }
+
+    public byte[] getBytes() {
+        return this.image.getBytes();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.image.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o != null && o instanceof StringKey) {
+            StringKey other = (StringKey) o;
+            return image.equals(other.image);
+        }
+        return false;
+    }
 }
