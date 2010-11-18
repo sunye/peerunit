8fe4d82254a3771418a908d49fadd0022864b3d3
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2010-11-18 11:05:33 +0000

    Range

--- trunk/PeerUnit/src/test/java/fr/inria/peerunit/base/RangeTest.java
+++ trunk/PeerUnit/src/test/java/fr/inria/peerunit/base/RangeTest.java
@@ -53,18 +53,53 @@ public class RangeTest {
     }
 
     @Test
-    public void testIntervalOneToMany() {
-        Range r = Range.fromString("13-*");
-        int[] wrong = {0, -1, 12, Integer.MIN_VALUE};
-        int[] right = {13, 32, 16, 20, 25, 29, 33,  Integer.MAX_VALUE};
+    public void testSimpleUnion() {
+        Range r = Range.fromString("1, 5");
+        int[] right = {1,5};
 
-        for (int each : wrong) {
+        for(int each : right) {
+            assertTrue(r.includes(each));
+        }
+    }
+
+    @Test
+    public void testRangeUnion() {
+        Range r = Range.fromString("1-5, 10-15");
+        int[] right = {1, 5, 10, 13, 15};
+        int[] wrong = {0, 6, 9, 16, Integer.MAX_VALUE, -1};
+
+        for(int each : right) {
+            assertTrue(r.includes(each));
+        }
+
+        for(int each : wrong) {
             assertFalse(r.includes(each));
         }
+    }
 
-        for (int each : right) {
-            assertTrue("Must include: " + each, r.includes(each));
+    @Test
+    public void testMixUnion() {
+        Range r = Range.fromString("1-5, 15");
+        int[] right = {1, 5, 15};
+        int[] wrong = {0, 6, 9, 16, Integer.MAX_VALUE, -1};
+
+        for(int each : right) {
+            assertTrue(r.includes(each));
         }
 
+        for(int each : wrong) {
+            assertFalse(r.includes(each));
+        }
     }
+
+    @Test
+    public void testAllUnion() {
+        Range star = Range.fromString("5, 10-15, *");
+        assertTrue(star.includes(0));
+        assertTrue(star.includes(Integer.MAX_VALUE));
+        assertTrue(star.includes(42));
+        assertFalse(star.includes(-1));
+        assertFalse(star.includes(Integer.MIN_VALUE));
+    }
+
 }
