c617fd6964b636b699aed925a82341ecdd9664e6
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2010-12-13 21:02:54 +0000

    TestStartCluster clean code

--- HadoopTest/src/main/java/test/TestStartCluster.java
+++ HadoopTest/src/main/java/test/TestStartCluster.java
@@ -18,82 +18,62 @@ import java.rmi.RemoteException;
 
 public class TestStartCluster extends StartClusterParent {
 
-    @TestStep(order=1, timeout = 100000, range = "0")
+	@TestStep(order=1, timeout = 100000, range = "0")
     public void startNameNode() {
 
     	initNN();
 
-   }
+    }
 
     @TestStep(order=2, timeout=100000, range="1")
     public void startSNameNode() {
 
-	initSNN();
+    	initSNN();
 
     }
 
     @TestStep(order=3, timeout = 100000, range = "*")
     public void startDataNode() {
 
-	initDN();
+    	initDN();
 
     }
 
     @TestStep(order=4, timeout = 100000, range = "0")
-    public void startJobTracker() {
+    public void startJobTracker() throws Exception {
 
 //    	initJT();
 
-	log.info("Starting JobTracker!");
+    	log.info("Starting JobTracker!");
 
-
-        try {
-                String command = "/home/ppginf/michela/hadoop-0.20.2/bin/start-job.sh";
-                final Process process = Runtime.getRuntime().exec(command);
-        } catch (Exception e) {
-
-		log.info("Error starting JobTracker:");
-
-                log.warning(e.toString());
-
-        }
-
+        String command = "/home/ppginf/michela/hadoop-0.20.2/bin/start-job.sh";
+        final Process process = Runtime.getRuntime().exec(command);
+        
     }
 
     @TestStep(order=5, timeout = 100000, range = "*")
-    public void startTaskTracker() {
+    public void startTaskTracker() throws Exception {
 
+    	log.info("Starting TaskTracker!");
+    	
+        String command = "/home/ppginf/michela/hadoop-0.20.2/bin/start-task.sh";
+        final Process process = Runtime.getRuntime().exec(command);
 
-	log.info("Starting TaskTracker!");
+//	 	initTT();
 
-	try {
-                String command = "/home/ppginf/michela/hadoop-0.20.2/bin/start-task.sh";
-                final Process process = Runtime.getRuntime().exec(command);
-        } catch (Exception e) {
+    }
 
-		log.info("Error starting TaskTracker:");
+    @TestStep(order=6, timeout = 440000, range = "0")
+    public void sendJob() {
 
-                log.warning(e.toString());
+    	runPiEstimator piest = new runPiEstimator();
+    	piest.run();
 
-        }
+    }
 
-//	 initTT();
+    @AfterClass(range="*", timeout = 100000)
+    public void stopCluster() {
 
-   }
-
-   @TestStep(order=6, timeout = 440000, range = "0")
-   public void sendJob() {
-
-	runPiEstimator piest = new runPiEstimator();
-
-	piest.run();
-
-   }
-
-   @AfterClass(range="*", timeout = 100000)
-   public void stopCluster() {
-
-
-   }
+    }
 
 }
