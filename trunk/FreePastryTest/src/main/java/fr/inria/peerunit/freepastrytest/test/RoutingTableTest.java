b9e51de6f4f7d0fd396c550c25885e1719abead1
sunye <sunye@527d5a51-f140-0410-ba99-e037b0884c56>
2010-12-16 15:05:37 +0000

    improvements on freepastrytest

--- trunk/FreePastryTest/src/main/java/fr/inria/peerunit/freepastrytest/test/RoutingTableTest.java
+++ trunk/FreePastryTest/src/main/java/fr/inria/peerunit/freepastrytest/test/RoutingTableTest.java
@@ -4,20 +4,13 @@
  */
 package fr.inria.peerunit.freepastrytest.test;
 
-import fr.inria.peerunit.freepastrytest.model.RemoteModel;
-import fr.inria.peerunit.freepastrytest.model.RemoteModelImpl;
-import fr.inria.peerunit.freepastrytest.model.Model;
+import fr.inria.peerunit.dhtmodel.RemoteModel;
+import fr.inria.peerunit.dhtmodel.Model;
 import fr.inria.peerunit.parser.TestStep;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,30 +20,31 @@ import java.util.logging.Logger;
  */
 public class RoutingTableTest extends AbstractFreePastryTest {
 
+
+    /**
+     * Logger
+     */
     private static final Logger LOG = Logger.getLogger(RoutingTableTest.class.getName());
+
+    /**
+     * Port for RMI Registry
+     */
     private static final int PORT = 8282;
+
     private Model model;
     private RemoteModel remoteModel;
-    private Set<String> neighbors = new HashSet<String>();
+    private Registry registry;
 
-    @TestStep(range = "6", order = 1)
-    public void startModel() throws RemoteException, UnknownHostException {
-        RemoteModelImpl rmi = new RemoteModelImpl();
-        RemoteModel stub = (RemoteModel) UnicastRemoteObject.exportObject(rmi, 0);
-        Registry registry = LocateRegistry.createRegistry(PORT);
-        registry.rebind("Model", stub);
-        this.put(1, InetAddress.getLocalHost().getHostName());
 
-        model = new Model(rmi);
-        model.start();
 
-    }
 
     @TestStep(range = "*", order = 2, timeout = 40000)
     public void lookupModel() throws RemoteException, NotBoundException {
 
         String hostName = (String) this.get(1);
-        Registry registry = LocateRegistry.getRegistry(hostName, PORT);
+        LOG.info(hostName);
+
+        registry = LocateRegistry.getRegistry(hostName, PORT);
         remoteModel = (RemoteModel) registry.lookup("Model");
 
         assert remoteModel != null;
@@ -58,52 +52,54 @@ public class RoutingTableTest extends AbstractFreePastryTest {
 
     @TestStep(range = "0", timeout = 40000, order = 3)
     @Override
-    public void startBootstrap() throws UnknownHostException, IOException,
-            InterruptedException {
+    public void startBootstrap() throws Exception {
 
         super.startBootstrap();
     }
 
     @TestStep(range = "1-*", timeout = 100000, order = 4)
     @Override
-    public void startingNetwork() throws InterruptedException,
-            UnknownHostException, IOException {
+    public void startingNetwork() throws Exception {
 
         super.startingNetwork();
     }
 
     @TestStep(range = "*", order = 5)
     public void nodeCreation() throws RemoteException {
+        LOG.log(Level.INFO, "My id: {0}", peer.getId());
+        for (String each : peer.getRoutingTable()) {
+            LOG.log(Level.INFO, "Neighbor: {0}", each);
+        }
+
 
         remoteModel.newNode(peer.getId());
     }
 
-    @TestStep(range = "*", order = 6)
+    @TestStep(range = "*", order = 6, timeout = 110000)
+    public void stabilize() throws InterruptedException {
+        Thread.sleep(100000);
+    }
+
+
+    @TestStep(range = "*", order = 7)
     public void updateModel() throws RemoteException {
 
         LOG.log(Level.INFO, "Neighbors size: {0}", peer.getRoutingTable().size());
-        remoteModel.updateNode(peer.getId(),peer.getRoutingTable() );
+        remoteModel.updateNode(peer.getId(), peer.getRoutingTable());
     }
 
-    @TestStep(range = "6", order = 7)
-    public void unicity() {
-        LOG.log(Level.INFO,"Unicity Test");
+    @TestStep(range = "*", order = 14, timeout = 200000)
+    public void print() throws InterruptedException, RemoteException {
+        peer.print();
 
-        assert model.unicity() : "Unicity";
-    }
-
-    @TestStep(range = "6", order = 9)
-    public void distance() {
-        LOG.log(Level.INFO,"Unicity Test");
-
-        assert model.distance() : "Unicity";
     }
 
 
+    @TestStep(range = "*", order = 22)
+    public void quit() throws RemoteException, NotBoundException {
+        peer.leave();
+        //UnicastRemoteObject.unexportObject(remoteModel, true);
 
-    @TestStep(range = "6", order = 20)
-    public void print() {
-        model.print();
-        model.stop();
+
     }
 }
