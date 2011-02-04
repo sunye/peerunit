9d16f56e9e717a8999005aa734cd9311ab6bdecf
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-02-04 01:00:21 +0000

    Local execution

--- HadoopTest/run-coordinator.sh
+++ HadoopTest/run-coordinator.sh
@@ -1,3 +1,4 @@
 # Coordenador
-~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+#~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
 
