08da1f779510597b0cfb42a00076f98e5c299b9b
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-03-02 02:01:41 +0000

    New runners...

---
+++ branches/albonico/HadoopTest/run-coordinator.sh
@@ -0,0 +1,6 @@
+# Coordenador
+#~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+java=/usr/lib/jvm/java-6-sun/bin/java
+
+$java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+
