49624f14a41fca6dcc4fefbfbd41d89a7da793a4
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-03-02 02:01:11 +0000

    Deleting runners...

--- branches/albonico/HadoopTest/run-coordinator.sh
+++
@@ -1,4 +0,0 @@
-# Coordenador
-#~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
-java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
-
--- branches/albonico/HadoopTest/run-remote-tester.sh
+++
@@ -1,5 +0,0 @@
-# Print the remote name
-echo "Remote Shell Command on $1"
-
-# Execution
-ssh $1 "dirpeerunit=`pwd` && hadoophome=/home/michel/hadoop-0.20.2/ && cd $dirpeerunit && java -Xmx1000m -Dcom.sun.management.jmxremote -Dhadoop.log.dir=$dirpeerunit/logs -Dhadoop.log.file=hadoop.log -Dhadoop.home.dir=$dirpeerunit -Dhadoop.id.str=hadoop -Dhadoop.root.logger=INFO,DRFA -classpath /usr/lib/jvm/java-6-sun/lib/tools.jar:$hadoophome:$dirpeerunit/target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner test.TestStartCluster &"
