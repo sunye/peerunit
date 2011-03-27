30a3b6da97ca3ed4b7954e58191183c63d559bc6
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-03-27 18:34:05 +0000

    Cleaning

--- branches/albonico/OpenChordTest/src/main/java/fr/inria/peerunit/openchordtest/util/Peer.java
+++
@@ -1,8 +0,0 @@
-package fr.inria.peerunit.openchordtest.util;
-
-public interface Peer {
-	 public void join();
-	 public void leave();
-	 public void put(int key, String value);
-	 public String get(int key);	
-}
