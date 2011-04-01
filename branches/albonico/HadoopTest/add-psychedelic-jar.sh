eaf8a1091f0798e08965c3551a8ab39c4f098007
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-04-01 12:52:54 +0000

    Adicionar o Psychedelic ao Maven

--- branches/albonico/HadoopTest/add-psychedelic-jar.sh
+++ branches/albonico/HadoopTest/add-psychedelic-jar.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=fr.inria.peerunit -DartifactId=Psychedelic -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=/home/michel/workspace-eclipse/albonico/Psychedelic/target/Psychedelic-1.0-SNAPSHOT.jar
+mvn install:install-file -DgroupId=fr.inria.peerunit -DartifactId=Psychedelic -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=../Psychedelic/target/Psychedelic-1.0-SNAPSHOT.jar
