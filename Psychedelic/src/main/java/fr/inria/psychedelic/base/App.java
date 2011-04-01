3e1a20c0a19c5c4d947c3baa8a7042386f47c737
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-04-01 12:48:41 +0000

    Psychedelic Add

---
+++ Psychedelic/src/main/java/fr/inria/psychedelic/base/App.java
@@ -0,0 +1,91 @@
+package fr.inria.psychedelic.base;
+
+import fr.inria.psychedelic.Psychedelic;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * 
+ *
+ */
+public class App {
+
+   // private static String source = "target/classes/fr/inria/psychedelic/sample/PanisCircenses.class";
+   // private static String target = "target/mutants/";
+	
+	private static String source;
+	private static String target;
+	private static ArrayList outputList = new ArrayList();
+	
+	public void runMutation(String src, String targ) throws FileNotFoundException, IOException {
+		
+		source = src;
+		target = targ;
+		
+		// Get class for mutation
+		String[] tokens = source.split("/");
+		String mutClass = tokens[tokens.length - 1]; 
+		
+		// Step 1
+        // Generate prospects
+        List<Prospect> prospects;
+        File f = new File(source);
+        FileInputStream input = new FileInputStream(f);
+        prospects = Psychedelic.prospectsFor(input);
+        input.close();
+
+        // Step 2
+        // Apply prospects
+        // 1 prospect == 1 .class file.
+        File dir = new File(target);
+        dir.mkdirs();
+        int i = 0;
+        for(Prospect each : prospects) {
+        	
+            input = new FileInputStream(source);
+           
+            //File outDir = new File(dir.getAbsolutePath()+"/"+i+++"/");
+            File outDir = new File(dir.getPath()+"/"+ i +"/");
+            outDir.mkdirs();
+            
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(dir.getPath()+"/"+ i +"/"+mutClass));
+            Psychedelic.applyProspect(input, out, each);
+            outputList.add(dir.getPath()+"/"+ i +"/"+mutClass);
+            out.close();
+            input.close();
+            
+            //System.out.println(each);
+            
+            i++;
+        }        
+		
+	}
+	
+	public ArrayList getOutputList() {
+		
+		return outputList;
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			App mutationApp = new App();
+	    	mutationApp.runMutation(args[0], args[1]);
+	    	System.out.println("Mutations successfully generated!");
+		} catch (FileNotFoundException fnfe) {
+			System.out.println(fnfe.toString());
+		} catch (IOException ie) {
+			System.out.println(ie.toString());
+		}
+		
+	}
+
+}
