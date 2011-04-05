f23d5c7a8d3e45d47178bdaa628081f340bffa35
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-04-05 19:48:05 +0000

    WordCount Test

---
+++ HadoopTest/src/main/java/test/TestWordCountResult.java
@@ -0,0 +1,61 @@
+package test;
+
+/**
+ * @author albonico  
+ */
+
+// My classes
+//import examples.PiEstimator;
+import load.StartClusterParent;
+
+// PeerUnit classes
+import fr.inria.peerunit.parser.TestStep;
+import fr.inria.peerunit.parser.AfterClass;
+import fr.inria.peerunit.tester.Assert;
+
+// Java classes
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class TestWordCountResult extends TestStartCluster {
+
+	
+	/**
+	 * Start at TestSteps 5
+	 */
+	
+	 @TestStep(order=5, timeout = 440000, range = "0")
+	 public void sendFile() throws Exception, InterruptedException {
+	    	
+		log.info("Putting file!");
+	   	putFileHDFS("/home/michel/workspace-eclipse/albonico/HadoopTest/teste","/input/");
+	    
+	 }
+	
+    @TestStep(order=6, timeout = 440000, range = "0")
+    public void runWCount() throws Exception, InterruptedException {
+    	
+    	runJob("wordcount");
+    	
+    }
+    
+    
+    @TestStep(order=7, timeout = 440000, range = "0")
+    public void assertResult() throws Exception, InterruptedException {
+    	
+    	ArrayList al = new ArrayList();
+    	al.add("michel	2");
+    	al.add("albonico	1");
+    	
+    	// Verify output
+    	validateJobOutput("/output/", al);    	
+    	
+    }
+    
+
+}
