2e3ae8a9ad42a4eb16890ab0674f1e08032d085a
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-04-13 12:15:47 +0000

    run-mutations.sh: shell script to run mutations automatically

--- branches/albonico/HadoopTest/run-coordinator.sh
+++ branches/albonico/HadoopTest/run-coordinator.sh
@@ -2,5 +2,5 @@
 #~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
 java=/usr/lib/jvm/java-6-sun/bin/java
 
-$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.CoordinatorRunner
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.CoordinatorRunner & 
 
