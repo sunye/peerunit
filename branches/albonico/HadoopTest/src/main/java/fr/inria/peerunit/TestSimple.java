899fa4e705d93e2de737b2ec94fac7ff423f85d6
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-06-01 17:22:26 +0000

    PeerUnit strategy!

---
+++ branches/albonico/HadoopTest/src/main/java/fr/inria/peerunit/TestSimple.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.inria.peerunit;
+
+import fr.inria.peerunit.parser.TestStep;
+
+import java.rmi.registry.Registry;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sunye
+ */
+public class TestSimple extends AbstractMR {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOG = Logger.getLogger(TestSimple.class.getName());
+    
+    /**
+     * Port for RMI Registry
+     */
+    private static final int PORT = 8282;
+    private Registry registry;
+
+    private static int count = 0;
+
+
+    @TestStep(range = "*", order = 2, timeout = 40000)
+    public void firstStep() {
+
+       System.out.println("Hello!!!");
+       
+    }
+
+    @TestStep(range = "*", order = 3, timeout = 40000)
+    public void incrementCount() {
+        
+        count++;
+
+    }
+
+    @TestStep(range = "*", order = 4, timeout = 40000)
+    public void lastStep() {
+
+        System.out.println("Count is: " + count);
+        
+    }
+
+
+   
+}
