af7dbc89785eaa21fb7370ba7deb693fa396db47
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-06-10 18:51:14 +0000

    Script to run TestSimpleStrategy.

---
+++ branches/albonico/HadoopTest/run-strategy.sh
@@ -0,0 +1,9 @@
+# Coordenador
+#~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
+java=/usr/lib/jvm/java-6-sun/bin/java
+
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.CoordinatorRunner &
+
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner fr.inria.peerunit.TestSimple &
+
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner fr.inria.peerunit.TestSimple &
