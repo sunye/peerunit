6d366f807591d352989f58fb0e4c5c3e66f39f95
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2011-08-02 20:55:46 +0000

    PeeUnit trunk on albonico branch.

---
+++ branches/albonico/PeerUnit/src/site/resources/java/freepastry/TestNewJoin.java
@@ -0,0 +1,168 @@
+package freepastry.test;
+import static fr.inria.peerunit.test.assertion.Assert.fail;
+import static fr.inria.peerunit.test.assertion.Assert.inconclusive;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import rice.pastry.Id;
+import rice.pastry.NodeHandle;
+import fr.inria.peerunit.TestCaseImpl;
+import fr.inria.peerunit.parser.AfterClass;
+import fr.inria.peerunit.parser.BeforeClass;
+import fr.inria.peerunit.parser.TestStep;
+import fr.inria.peerunit.util.TesterUtil;
+import freepastry.Network;
+import freepastry.Peer;
+
+/**
+ * Test routing table update in an expanding system
+ * @author almeida
+ *
+ */
+public class TestNewJoin extends TestCaseImpl{
+	private static Logger log = Logger.getLogger(TestNewJoin.class.getName());
+
+	private static final int OBJECTS=TesterUtil.getObjects();
+
+	Peer peer=new Peer();
+
+	int sleep=TesterUtil.getSleep();
+
+	List<Id> firstSuccessors=new ArrayList<Id>();
+
+
+	@BeforeClass(place=-1,timeout=1000000)
+	public void bc(){
+		log.info("[PastryTest] Starting test peer  ");
+	}
+
+
+	@TestStep(place=-1,timeout=1000000, name = "action2", step = 0)
+	public void startingHalfNet(){
+
+		try {
+
+			if(this.getPeerName()%2!=0){
+				log.info("Joining in first");
+				Network net= new Network();
+				Thread.sleep(this.getPeerName()*1000);
+
+				if(!net.joinNetwork(peer, null,false, log)){
+					inconclusive("I couldn't join, sorry");
+				}
+				log.info("Getting cached boot "+net.getInetSocketAddress().toString());
+				log.info("Running on port "+peer.getPort());
+				log.info("Time to bootstrap");
+
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@TestStep(place=-1,timeout=1000000, name = "action4", step = 0)
+	public void testFind(){
+		try {
+			Thread.sleep(sleep);
+			if(this.getPeerName()%2!=0){
+				log.info("My ID "+peer.getId());
+				for(NodeHandle nd: peer.getRoutingTable()){
+					log.info("Successor NodeId "+nd.getId());
+					firstSuccessors.add(nd.getNodeId());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	@TestStep(place=-1,timeout=1000000, name = "action5", step = 0)
+	public void startingOtherHalfNet(){
+
+		try {
+
+			if(this.getPeerName()%2==0){
+				log.info("Joining in first");
+				Network net= new Network();
+				Thread.sleep(this.getPeerName()*1000);
+
+				if(!net.joinNetwork(peer, null,false, log)){
+					inconclusive("I couldn't join, sorry");
+				}
+				log.info("Getting cached boot "+net.getInetSocketAddress().toString());
+				log.info("Running on port "+peer.getPort());
+				log.info("Time to bootstrap");
+
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@TestStep(place=-1,timeout=1000000, name = "action7", step = 0)
+	public void testFindAgain(){
+		try {
+			if((this.getPeerName()%2!=0)){
+				List<NodeHandle> actuals;
+
+				//Iterations to clean the volatiles from the routing table
+				int timeToUpdate=0;
+				Id obj=null;
+				boolean tableUpdated=false;
+				while(!tableUpdated &&	timeToUpdate < TesterUtil.getLoopToFail()){
+					log.info("Verifying the "+timeToUpdate+" time ");
+					try {
+						Thread.sleep(1000);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					actuals= peer.getRoutingTable();
+
+					for(NodeHandle nd: actuals){
+						obj=nd.getNodeId();
+						log.info(" Successor NodeId "+obj);
+						if(!firstSuccessors.contains(obj)){
+							log.info("List updated, the verdict may be PASS ");
+							tableUpdated=true;
+						}
+					}
+
+					//Demanding the routing table update
+					peer.pingNodes();
+
+					timeToUpdate++;
+				}
+				if(!tableUpdated)
+					fail("Routing Table wasn't updated. Still finding all volatiles. Increase qty of loops.");
+				else
+					log.info("List updated, the verdict may be PASS. Table updated "+timeToUpdate+" times.");
+
+			}
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass(timeout=100000,place=-1)
+	public void end() {
+		log.info("[PastryTest] Peer bye bye");
+	}
+}
+
---
+++ branches/albonico/PeerUnit/src/test/java/fr/inria/peerunit/GlobalVariablesTest.java
@@ -0,0 +1,140 @@
+/*
+This file is part of PeerUnit.
+
+PeerUnit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PeerUnit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PeerUnit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.inria.peerunit;
+
+import fr.inria.peerunit.remote.GlobalVariables;
+import fr.inria.peerunit.util.TesterUtil;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sunye
+ */
+public class GlobalVariablesTest {
+
+    private GlobalVariables globals;
+    private static CoordinatorRunner runner;
+    private Registry registry;
+
+    public GlobalVariablesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        runner = new CoordinatorRunner(TesterUtil.instance);
+        runner.bindGlobals();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        runner.cleanAndUnbind();
+    }
+
+    @Before
+    public void setUp() throws RemoteException, NotBoundException {
+        registry = LocateRegistry.getRegistry(TesterUtil.instance.getRegistryPort());
+        globals = (GlobalVariables) registry.lookup("Globals");
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of put method, of class GlobalVariables.
+     */
+    @Test
+    public void testPut() throws Exception {
+        for (int i = 0; i < 50; i++) {
+            globals.put(new Integer(i), i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Integer value = (Integer) globals.get(i);
+            assertTrue(i == value.intValue());
+        }
+    }
+
+    /**
+     * Test of get method, of class GlobalVariables.
+     */
+    @Test
+    public void testGet() throws Exception {
+        for (int i = 0; i < 50; i++) {
+            globals.put(new Integer(i), i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Integer value = (Integer) globals.get(i);
+            assertTrue(i == value.intValue());
+        }
+    }
+
+    /**
+     * Test of getCollection method, of class GlobalVariables.
+     */
+    @Test
+    public void testGetCollection() throws Exception {
+        for (int i = 0; i < 50; i++) {
+            globals.put(new Integer(i), i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Integer value = (Integer) globals.get(i);
+            assertTrue(i == value.intValue());
+        }
+    }
+
+    /**
+     * Test of containsKey method, of class GlobalVariables.
+     */
+    @Test
+    public void testContainsKey() throws Exception {
+        for (int i = 0; i < 50; i++) {
+            globals.put(new Integer(i), i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Integer value = (Integer) globals.get(i);
+            assertTrue(i == value.intValue());
+        }
+    }
+
+    /**
+     * Test of clearCollection method, of class GlobalVariables.
+     */
+    @Test
+    public void testClearCollection() throws Exception {
+        for (int i = 0; i < 50; i++) {
+            globals.put(new Integer(i), i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Integer value = (Integer) globals.get(i);
+            assertTrue(i == value.intValue());
+        }
+    }
+}
