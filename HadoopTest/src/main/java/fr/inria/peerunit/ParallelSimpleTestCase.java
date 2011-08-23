19487fd09a46da3ab61dda1b711f0ef3253209f9
jeugenio <jeugenio@527d5a51-f140-0410-ba99-e037b0884c56>
2011-08-23 20:28:09 +0000

    Improved ParallelSimpleTestCase adding that one action must be executed by two testers.

--- HadoopTest/src/main/java/fr/inria/peerunit/ParallelSimpleTestCase.java
+++ HadoopTest/src/main/java/fr/inria/peerunit/ParallelSimpleTestCase.java
@@ -18,7 +18,7 @@ public class ParallelSimpleTestCase {
 
     private static final Logger LOG = Logger.getLogger(ParallelTestSimple.class.getName());
 
-    @TestStep(order = 0, range = "0", timeout = 40000)
+    @TestStep(order = 0, range = "0,1", timeout = 40000)
     public void a0() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             System.out.println("a0, order=1, range=0");
