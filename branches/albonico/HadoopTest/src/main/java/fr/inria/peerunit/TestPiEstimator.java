8affb26c2481a29427738b6876607116bb938f9f
jeugenio <jeugenio@527d5a51-f140-0410-ba99-e037b0884c56>
2012-02-22 23:42:28 +0000

    dependency using.

--- branches/albonico/HadoopTest/src/main/java/fr/inria/peerunit/TestPiEstimator.java
+++ branches/albonico/HadoopTest/src/main/java/fr/inria/peerunit/TestPiEstimator.java
@@ -6,42 +6,62 @@ package fr.inria.peerunit;
 // PeerUnit classes
 
 import fr.inria.peerunit.parser.TestStep;
+import fr.inria.peerunit.tester.Assert;
 import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestPiEstimator extends AbstractMR {
    
-    @TestStep(order = 1, timeout = 30000, range = "0")
-    public void a0() throws IOException, InterruptedException {
-        startMaster();
+    @TestStep(order = 1, range = "0", timeout = 60000)
+    public void a0() {
+        try {
+            startMaster();
+        } catch (RemoteException ex) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("RemoteException="+ex.toString());
+        } catch (IOException ex) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("IOException="+ex.toString());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("InterruptedException="+ex.toString());
+        }
+        finally {
+            System.out.println("finally");
+            Assert.fail();
+        }
+
     }
 
-    @TestStep(order = 2, timeout = 200000, range = "*")
+    @TestStep(order = 2, range = "*", depend = "a0", timeout = 120000)
     public void a1() throws IOException, InterruptedException {
         startWorkers();
     }
 
-    @TestStep(order = 3, range = "0", timeout = 3850000)
+    @TestStep(order = 3, range = "0", depend = "a1", timeout = 60000)
     public void a2() throws Exception, InterruptedException {
         sendJob();
     }
 
-    @TestStep(order = 3, range = "1", timeout = 120000)
+    @TestStep(order = 3, range = "0", depend = "a2", timeout = 60000)
     public void a3() throws Exception, InterruptedException {
         Thread.currentThread().sleep(10000);
         killWorker();
     }
 
-    @TestStep(order = 4, timeout = 10000, range = "0")
+    @TestStep(order = 4, range = "0", depend = "a2", timeout = 10000)
     public void a4() throws Exception, InterruptedException {
         assertResult();
     }
 
-    @TestStep(order = 5, timeout = 30000, range = "*")
+    @TestStep(order = 5, range = "*", depend = "a1", timeout = 60000)
     public void a5() throws IOException, InterruptedException {
         stopWorkers();
     }
   
-    @TestStep(order = 6, timeout = 30000, range = "0")
+    @TestStep(order = 6, range = "0", depend = "a0", timeout = 60000)
     public void a6() throws IOException, InterruptedException {
         stopMaster();
     }
