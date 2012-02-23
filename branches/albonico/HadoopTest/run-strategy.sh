0d21b644e6796bb8e72f195419e6664c4b380f2b
jeugenio <jeugenio@527d5a51-f140-0410-ba99-e037b0884c56>
2012-02-23 11:02:56 +0000

    peerunit.properties - only comments pom.xml - compiling with dependencies e changed PeerUnit dependency of ALBONICO to SNAPSHOT. run-strategy.sh - included var TESTE to specify the problem test.

--- branches/albonico/HadoopTest/run-strategy.sh
+++ branches/albonico/HadoopTest/run-strategy.sh
@@ -1,12 +1,14 @@
 # Coordenador
 #~/jre1.6.0_20/bin/java -classpath ../PeerUnit/target/PeerUnit-1.1-SNAPSHOT.jar fr.inria.peerunit.CoordinatorRunner &
 java=/usr/lib/jvm/java-6-sun/bin/java
+TESTE=fr.inria.peerunit.SimpleGlobalTestCase
+TESTE=fr.inria.peerunit.TestPiEstimator
 
 $java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.CoordinatorRunner &
 
-$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner fr.inria.peerunit.TestSimple &
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner $TESTE &
 
-$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner fr.inria.peerunit.TestSimple &
+$java -classpath ./target/HadoopTest-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.peerunit.TestRunner $TESTE &
 #echo "Running remote command on $1"
 
 #ssh $1 "cd ~/HadoopTest/ && ./run-local-tester.sh"
