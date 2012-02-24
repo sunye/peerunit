9a67d825550b3973fb047602b67fa4dc66cc661d
jeugenio <jeugenio@527d5a51-f140-0410-ba99-e037b0884c56>
2012-02-24 17:10:29 +0000

    code improviments in manipulation of Exceptions and resolved DataNode and SendJob problems.

--- HadoopTest/src/main/java/fr/inria/peerunit/TestPiEstimator.java
+++ HadoopTest/src/main/java/fr/inria/peerunit/TestPiEstimator.java
@@ -16,53 +16,89 @@ public class TestPiEstimator extends AbstractMR {
    
     @TestStep(order = 1, range = "0", timeout = 60000)
     public void a0() {
+        System.out.println("a0:");
         try {
             startMaster();
-        } catch (RemoteException ex) {
-            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("RemoteException="+ex.toString());
-        } catch (IOException ex) {
-            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("IOException="+ex.toString());
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("InterruptedException="+ex.toString());
-        }
-        finally {
-            System.out.println("finally");
+        } catch (Exception e) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, 
+                    null, e.getStackTrace().toString());
+            System.out.println("a0 Exception="+e.toString());
+            e.printStackTrace();
             Assert.fail();
         }
-
     }
 
     @TestStep(order = 2, range = "*", depend = "a0", timeout = 120000)
-    public void a1() throws IOException, InterruptedException {
-        startWorkers();
+    public void a1(){
+        System.out.println("a1:");
+        try {
+            startWorkers();
+        } catch (Exception e) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, 
+                    null, e.getStackTrace().toString());
+            System.out.println("a1 Exception="+e.toString());
+            e.printStackTrace();
+            Assert.fail();
+        }
     }
 
-    @TestStep(order = 3, range = "0", depend = "a1", timeout = 60000)
-    public void a2() throws Exception, InterruptedException {
-        sendJob();
+    @TestStep(order = 3, range = "0", depend = "a1", timeout = 600000)
+    public void a2() {
+        System.out.println("a2:");
+        /*
+        int i=0;
+        try {
+            i = System.in.read();
+        } catch (IOException ex) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("i="+i);
+         * 
+         */
+        try {
+            sendJob();
+        } catch (Exception e) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE,
+                    null, e.getStackTrace().toString());
+            System.out.println("a2 Exception="+e.toString());
+            e.printStackTrace();
+            Assert.fail();
+        }
+        System.out.println("a2 after sendJob()");
     }
 
-    @TestStep(order = 3, range = "0", depend = "a2", timeout = 60000)
-    public void a3() throws Exception, InterruptedException {
-        Thread.currentThread().sleep(10000);
-        killWorker();
+    @TestStep(order = 3, range = "1", depend = "a2", timeout = 60000)
+    public void a3() {
+        try {
+            System.out.println("a3():");
+            //Thread.currentThread().sleep(10000);
+            //killWorker();
+            stopWorkers();
+        } catch (Exception e) {
+            Logger.getLogger(TestPiEstimator.class.getName()).log(Level.SEVERE,
+                    null, e.getStackTrace().toString());
+            System.out.println("a3 Exception="+e.toString());
+            e.printStackTrace();
+            Assert.fail();
+        }
     }
 
     @TestStep(order = 4, range = "0", depend = "a2", timeout = 10000)
     public void a4() throws Exception, InterruptedException {
+        System.out.println("a4.read():");
+        //System.out.println(System.in.read());
         assertResult();
     }
 
     @TestStep(order = 5, range = "*", depend = "a1", timeout = 60000)
     public void a5() throws IOException, InterruptedException {
+        System.out.println("a5():");
         stopWorkers();
     }
   
     @TestStep(order = 6, range = "0", depend = "a0", timeout = 60000)
     public void a6() throws IOException, InterruptedException {
+        System.out.println("a6():");
         stopMaster();
     }
 }
