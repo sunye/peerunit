9aef0fcff2172c54e9aab9d1019d692b5e467943
albonico <albonico@527d5a51-f140-0410-ba99-e037b0884c56>
2012-03-05 23:28:05 +0000

    KillWorker function on AbstractMR.

--- HadoopTest/src/main/java/fr/inria/peerunit/HadoopTaskTrackerWrapper.java
+++ HadoopTest/src/main/java/fr/inria/peerunit/HadoopTaskTrackerWrapper.java
@@ -1,11 +1,15 @@
 package fr.inria.peerunit;
 
 import java.io.IOException;
+import java.security.Security;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TaskAttemptID;
 import org.apache.hadoop.mapred.TaskTracker;
+import org.hsqldb.Session;
 
 /**
  * 
@@ -16,34 +20,61 @@ public class HadoopTaskTrackerWrapper {
 	private static final Logger LOG = Logger
 			.getLogger(HadoopTaskTrackerWrapper.class.getName());
 
-	private TaskTracker taskTracker;
-	private Thread taskTrackerThread;
-    private final Configuration configuration;
+	private static TaskTracker taskTracker;
+	private static Thread taskTrackerThread;
+        private final Configuration configuration;
 
 	public HadoopTaskTrackerWrapper(Configuration configuration) {
         
-
-        this.configuration = configuration;
+            this.configuration = configuration;
 
 	}
 
 	protected void start() throws IOException, InterruptedException {
-		// TaskTrackers
-		LOG.info("Starting TaskTracker!");
-
+	
+                // Create a thread to initialize TaskTracker.
 		taskTrackerThread = new Thread(new TaskTrackerThread());
 		taskTrackerThread.start();
+               // taskTracker.wait(5000);
 		Thread.yield();
+                
 	}
+        
+        protected void kill() throws Exception {
+            
+            try {
+                 //   Security.setProperty("RuntimePermission", "stopThread");
+                    taskTrackerThread.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            
+        }
 
-	protected void stop() throws IOException {
+	protected void stop() throws IOException, Exception {
 
 		LOG.info("Stopping TaskTracker...");
-		// TTracker.shutdown();
-		if (taskTrackerThread.isAlive()) {
+                
+                //System.exit(0); // Mata o testador
+                
+                taskTracker.shutdown();
+                if (taskTrackerThread.isAlive()) {
+                   //LOG.info("TaskTracker thread is alive!");
+                   taskTrackerThread.interrupt();
+                } else {
+                   //LOG.info("TaskTracker thread is not alive!");
+                }
+                
+	
+         /**
+                if (taskTrackerThread.isAlive()) {
 			taskTrackerThread.interrupt();
                         LOG.info("TaskTracker is stopped!");
-		}
+		} else {
+                    LOG.info("TaskTracker is not alive.");
+                    throw new Exception(); 
+                }
+          */      
 	}
 
 
